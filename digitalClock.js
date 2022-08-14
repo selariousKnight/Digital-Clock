fe51065d7652ce3b1abdae96f8c74dea7ec4bcbc
selariousKnight <98381961+selariousKnight@users.noreply.github.com>
2022-08-14 16:40:30 +0530

    Create digitalClock.js

---
+++ digitalClock.js
@@ -0,0 +1,29 @@
+let hoursEle = document.getElementById("hour");
+let minutesEle = document.getElementById("minutes");
+let secondsEle = document.getElementById("seconds");
+let amOrPmEle = document.getElementById("amPm");
+function updateClock(){
+    let presentTime = new Date()
+    let hours = presentTime.getHours()
+    let minutes = presentTime.getMinutes()
+    let seconds = presentTime.getSeconds()
+    let amOrPm = "Am"
+    if (hours > 12){
+        hours=hours-12
+        amOrPm ="Pm"
+    }
+    else{
+        amOrPm = "Am"
+    }
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    hoursEle.textContent = hours;
+    minutesEle.textContent = minutes;
+    secondsEle.textContent = seconds;
+    amOrPmEle.textContent = amOrPm;
+    setTimeout(()=>{
+        updateClock();
+    },1000)
+} 
+updateClock()
